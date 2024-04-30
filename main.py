0d2d91b8423b4c65e7ef277e3b38c44aa3eb017d
Kevin Gillespie <kevinragillespie@gmail.com>
2024-04-30 07:03:56 +0100

    final push

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
+def sort_on(dict):
+    return dict["count"]
+
 def getLetterCountForBookContent(bookContent):
-    word_dict = {}
+    letter_dict = {}
+    letter_list = []
     for character in list(bookContent.lower()):
-        word_dict.update({character: word_dict.get(character, 0) + 1})
-    return word_dict.
+        letter_dict.update({character : letter_dict.get(character, 0) + 1 })
+    for letter in letter_dict:
+        letter_list.append({"character" : letter, "count": letter_dict.get(letter)})
+    letter_list.sort(reverse=True, key=sort_on)
+    for letter_entry in letter_list:
+        if(letter_entry["character"].isalpha()):
+            print(f"The '{letter_entry["character"]}' character was found {letter_entry["count"]} times")
 
 def getWordCountForBookContent(bookContent):
-    words = bookContent.split()
-    print(len(words))
+    return len(bookContent.split())
 
 def readBook(pathToBook):
     file_contents = ""
     with open(pathToBook) as f:
         file_contents = f.read()
-        print(file_contents)
     return file_contents
 
 def buildBookReport(pathToBook):
     bookContent = readBook(pathToBook)
     print (f"--- Begin report of {pathToBook} ---")
-    print(f"{getWordCountForBookContent(bookContent)}")
+    print(f"{getWordCountForBookContent(bookContent)} words found in the document")
+    print("")
+    getLetterCountForBookContent(bookContent)
+    print("--- End Report ---")
 
 def main():
-    bookContent = readBook("books/frankenstein.txt")
-    getWordCountForBookContent(bookContent)
-    getLetterCountForBookContent(bookContent)
+    pathToBook = "books/frankenstein.txt"
     buildBookReport(pathToBook)
 
 main()
